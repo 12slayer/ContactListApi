1c28b4961f7879b372487e44b8d407a121929e81
12slayer <sushantking120@gmail.com>
2021-05-27 20:21:21 +0545

    depolying to the heroku

--- ContactsApi/settings.py
+++ ContactsApi/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import sys
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,4 +144,4 @@ JWT_SECRET_KEY = "something"
 
 STATIC_URL = '/static/'
 
-
+django_heroku.settings(locals())
